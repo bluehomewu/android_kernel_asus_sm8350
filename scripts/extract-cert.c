56bc28cd9e35fa856b0de2b757a4ba9c5b11df31
Matthias Maennich <maennich@google.com>
2020-11-26 10:08:38 +0000

    ANDROID: extract-cert: omit PKCS#11 support if building against BoringSSL
    BoringSSL does not implement the ENGINE API. In Android we do not seem
    to rely on the PKCS#11 -> DER extraction. Hence, make this conditional
    on the SSL library used.
    
    Bug: 135570712
    Signed-off-by: Matthias Maennich <maennich@google.com>
    Change-Id: I84af6633dd470083497087c7dd1a2734480e2b0e

--- scripts/extract-cert.c
+++ scripts/extract-cert.c
@@ -59,6 +59,7 @@ static void display_openssl_errors(int l)
 	}
 }
 
+#ifndef OPENSSL_IS_BORINGSSL
 static void drain_openssl_errors(void)
 {
 	const char *file;
@@ -68,6 +69,7 @@ static void drain_openssl_errors(void)
 		return;
 	while (ERR_get_error_line(&file, &line)) {}
 }
+#endif
 
 #define ERR(cond, fmt, ...)				\
 	do {						\
@@ -122,7 +124,10 @@ int main(int argc, char **argv)
 		fclose(f);
 		exit(0);
 	} else if (!strncmp(cert_src, "pkcs11:", 7)) {
-#if !defined(OPENSSL_NO_ENGINE) && OPENSSL_VERSION_MAJOR < 3
+#ifdef OPENSSL_IS_BORINGSSL
+		ERR(1, "BoringSSL does not support extracting from PKCS#11");
+		exit(1);
+#elif !defined(OPENSSL_NO_ENGINE) && OPENSSL_VERSION_MAJOR < 3
 		ENGINE *e;
 		struct {
 			const char *cert_id;
