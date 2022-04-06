b8a1d46135c988134b4df0868c9e863df3c45447
Surabhi Vishnoi <quic_svishnoi@quicinc.com>
2022-04-06 14:23:05 +0530

    qcacmn: Allow rx UDP packets during roaming before peer map is received
    In VoIP cases, no or minimal packet drop is expected during
    roaming. Currently, data packets are dropped in host if they
    are received after roam sync indication from firmware and before
    peer map indication is received due to invalid peer.
    This change allows rx UDP data packets during roaming before
    peer map indication is received from FW if peer's auth_status
    received in roam_sync_indication from FW is authorised.
    
    Change-Id: Ic518b7cd0aef48a2b0f8c923a2e20838b07f3d1f
    CRs-Fixed: 3168603
    (cherry picked from commit e1faf4064cefd5034ed2f5e6a75a3400414744ef)

--- drivers/staging/qca-wifi-host-cmn/dp/wifi3.0/dp_types.h
+++ drivers/staging/qca-wifi-host-cmn/dp/wifi3.0/dp_types.h
@@ -2541,6 +2541,15 @@ struct dp_pdev {
 
 struct dp_peer;
 
+#ifdef DP_RX_UDP_OVER_PEER_ROAM
+#define WLAN_ROAM_PEER_AUTH_STATUS_NONE 0x0
+/**
+ * This macro is equivalent to macro ROAM_AUTH_STATUS_AUTHENTICATED used
+ * in connection mgr
+ */
+#define WLAN_ROAM_PEER_AUTH_STATUS_AUTHENTICATED 0x2
+#endif
+
 /* VDEV structure for data path state */
 struct dp_vdev {
 	/* OS device abstraction */
@@ -2786,6 +2795,11 @@ struct dp_vdev {
 	qdf_atomic_t ref_cnt;
 	qdf_atomic_t mod_refs[DP_MOD_ID_MAX];
 	uint8_t num_latency_critical_conn;
+
+#ifdef DP_RX_UDP_OVER_PEER_ROAM
+	uint32_t roaming_peer_status;
+	union dp_align_mac_addr roaming_peer_mac;
+#endif
 };
 
 
