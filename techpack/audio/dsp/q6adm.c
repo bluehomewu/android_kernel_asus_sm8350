0345424321715bd8d85e46b7329ddff0c19281c4
EdwardWu <bluehome.wu@gmail.com>
2026-06-02 20:35:12 +0800

    techpack: audio: Import ASUS changes from 18.1055.2305.249
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- techpack/audio/dsp/q6adm.c
+++ techpack/audio/dsp/q6adm.c
@@ -3247,9 +3247,9 @@ int adm_open_v2(int port_id, int path, int rate, int channel_mode, int topology,
 					ec_ref_port_cfg->sampling_rate :
 					this_adm.ec_ref_rx_sampling_rate;
 
-	pr_debug("%s:port %#x path:%d rate:%d mode:%d perf_mode:%d,topo_id %d\n",
+	printk("%s:port %#x path:%d rate:%d mode:%d perf_mode:%d,topo_id %d, bit_width %d\n",
 		 __func__, port_id, path, rate, channel_mode, perf_mode,
-		 topology);
+		 topology, bit_width);
 
 	port_id = q6audio_convert_virtual_to_portid(port_id);
 	port_idx = adm_validate_and_get_port_index(port_id);
