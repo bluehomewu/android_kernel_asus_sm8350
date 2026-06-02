10ddf391fe437dafe59f98022d0b53ef757f0f65
EdwardWu <bluehome.wu@gmail.com>
2026-06-02 22:16:05 +0800

    qcacld: Fix fw-api include dependencies
    Follow the LineageOS sm8350 wlan layout for newer fw-api headers. Keep enet.h unchanged, include it before pktlog clears __ATTRIB_PACK in wma_data.c, and provide A_OFFSETOF from osapi_linux.h for wlan_defs.h.
    
    LineageOS/android_kernel_asus_sm8350@5840d3cbf30ad65de4eaa8a770d6917f4a964178
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- drivers/staging/qcacld-3.0/uapi/linux/osapi_linux.h
+++ drivers/staging/qcacld-3.0/uapi/linux/osapi_linux.h
@@ -73,6 +73,7 @@
 #define A_PRINTF(args ...) \
 	QDF_TRACE_ERROR(QDF_MODULE_ID_QDF, args)
 #define A_SNPRINTF(buf, len, args ...)   snprintf(buf, len, args)
+#define A_OFFSETOF(type, field)   offsetof(type, field)
 
 /*
  * Timer Functions
