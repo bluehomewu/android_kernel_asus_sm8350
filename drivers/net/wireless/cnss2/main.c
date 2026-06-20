a21f85c2b8e2365e166cecc1c94a7b23d66cc22b
EdwardWu <bluehome.wu@gmail.com>
2026-06-20 23:32:51 +0800

    cnss2: Make ASUS antenna switch sysfs write-only
    do_wifi_antenna_switch only implements a store callback, but it was registered with read permissions.
    
    260619_logcat.log starts with the kernel warning: Attribute do_wifi_antenna_switch: read permission without 'show'. Use a write-only mode so sysfs no longer exposes an unreadable attribute.
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- drivers/net/wireless/cnss2/main.c
+++ drivers/net/wireless/cnss2/main.c
@@ -3158,7 +3158,7 @@ static ssize_t do_wifi_antenna_switch_store(struct device *dev,
 	return count;
 }
 
-static DEVICE_ATTR(do_wifi_antenna_switch, 0644, NULL,
+static DEVICE_ATTR(do_wifi_antenna_switch, 0200, NULL,
 		   do_wifi_antenna_switch_store);
 #endif
 
